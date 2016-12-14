164c29d7e2d10863fc34b541179b0ef4070ec723
Dylan Lewis <dylanlewis89@gmail.com>
2016-12-13 23:38:33 -0500

    allow prototype to hit api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'http://conservewith.us'
+    origins 'localhost:8080', 'http://conservewith.us', 'http://prototype.conservewith.us'
 
     resource '*',
       headers: :any,
