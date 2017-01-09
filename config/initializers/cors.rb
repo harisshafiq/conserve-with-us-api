4fcc1116dfe8123bd74ef9c4a09b490093ead231
Dylan Lewis <dylanlewis89@gmail.com>
2017-01-09 18:35:07 -0500

    allow requests from dev instance

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'http://conservewith.us', 'http://prototype.conservewith.us'
+    origins 'localhost:8080', 'http://conservewith.us', 'http://prototype.conservewith.us', 'http://dev.conservewith.us'
 
     resource '*',
       headers: :any,
